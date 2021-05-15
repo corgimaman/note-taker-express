4b8b838f930c1470b1cabdeffb876f89afa04841
romantic <ida.shalilian@gmail.com>
2021-05-15 14:06:00 -0500

    added env port

--- controllers/api.js
+++ controllers/api.js
@@ -46,21 +46,21 @@ module.exports = function(app) {
     });
 
 
-    app.delete('/api/notes/:id', function(req,res) {
-        console.log('id to delte!', req.params.id)
-        const deleteMe = req.params.id;
-        //make an empty array and put everybody back in minus the id i wanted to delete
-        read file -> array -> make constant
-        /* Check each character routeName and see if the same as "chosen"
-         If the statement is true, send the character back as JSON,
-         otherwise tell the user no character was found */
+    // app.delete('/api/notes/:id', function(req,res) {
+    //     console.log('id to delte!', req.params.id)
+    //     const deleteMe = req.params.id;
+    //     //make an empty array and put everybody back in minus the id i wanted to delete
+    //     read file -> array -> make constant
+    //     /* Check each character routeName and see if the same as "chosen"
+    //      If the statement is true, send the character back as JSON,
+    //      otherwise tell the user no character was found */
       
-        for (let i = 0; i < req.length; i++) {
-          if (deleteMe === characters[i].routeName) {
-            return res.json(characters[i]);
-          }
-        }
+    //     for (let i = 0; i < req.length; i++) {
+    //       if (deleteMe === characters[i].routeName) {
+    //         return res.json(characters[i]);
+    //       }
+    //     }
       
-        return res.json(false);
-    })
+    //     return res.json(false);
+    // })
 }

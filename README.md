9979aadddfc2fa9c7ad73a9164a8615c99fe7643
romantic <ida.shalilian@gmail.com>
2021-05-16 12:46:26 -0500

    asdfg

--- README.md
+++ README.md
@@ -1,19 +1,4 @@
 # Note Taker
 
-this project is all about getting routes
+A note taking app using Node.js, Express.js, and Heroku.
 
-html routes and api routes - server should make it clear that these are separate.
-
-https://www.npmjs.com/package/uniqid
-
-1. get route for get /notes html
-2. get * for index.html (* symbol is wildcard, can also be nothing)
-3. api route - read request to return information from our database
-4. api route - post route 
-
-app get
-app.post
-
-fill in functions
-
-look at starwars activities
--- controllers/api.js
+++ controllers/api.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
 
-    const DB = './db/db2.json'
+    const DB = './db/db.json'
     const fs = require('fs');
 
     app.get('/api/notes', (req, res) => {
@@ -15,11 +15,10 @@ module.exports = function(app) {
     });
 
     app.post('/api/notes', (req, res) => {
-        console.log(req.body);
         fs.readFile(DB, 'utf8', function(err, data) {
             if(err) {
                 return console.log(err);
-            }
+            };
 
             let parsed = JSON.parse(data);
             let newNote = req.body;
@@ -46,21 +45,43 @@ module.exports = function(app) {
     });
 
 
-    // app.delete('/api/notes/:id', function(req,res) {
-    //     console.log('id to delte!', req.params.id)
-    //     const deleteMe = req.params.id;
-    //     //make an empty array and put everybody back in minus the id i wanted to delete
-    //     read file -> array -> make constant
-    //     /* Check each character routeName and see if the same as "chosen"
-    //      If the statement is true, send the character back as JSON,
-    //      otherwise tell the user no character was found */
+    app.delete('/api/notes/:id', (req,res) => {
+        deleteMe = parseInt(req.params.id);
+        var data = fs.readFile(DB);
+        var parsed = JSON.parse(data);
+
+        parsed.filter((parsed) => { return parsed.id !== deleteMe });
+
+        fs.writeFile(DB, JSON.stringify(parsed, null, 2))
+        return res.json(false);
+    });
+        // fs.writeFile(DB, 'utf8', function(err, data) {
+        //     if(err) {
+        //         console.log(err);
+        //         return;
+        //     };
+        //     res.json(deleteNote);
+        // });
+        // const notes = JSON.parse(fs.readFile(DB, 'utf8', {
+        //     if (err) {
+        //         console.log(err)
+        //         return;
+        //    }
+        //    data = JSON.parse(data);
+        //    res.json(data); 
+        // }))
+        //make an empty array and put everybody back in minus the id i wanted to delete
+        //read file -> array -> make constant
+        /* Check each character routeName and see if the same as "chosen"
+         If the statement is true, send the character back as JSON,
+         otherwise tell the user no character was found */
       
-    //     for (let i = 0; i < req.length; i++) {
-    //       if (deleteMe === characters[i].routeName) {
-    //         return res.json(characters[i]);
-    //       }
-    //     }
+        // for (let i = 0; i < req.length; i++) {
+        //   if (deleteMe === characters[i].routeName) {
+        //     return res.json(characters[i]);
+        //   }
+        // }
       
-    //     return res.json(false);
-    // })
+        // return res.json(false);
+   // });
 }

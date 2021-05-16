744f36fd3994a463618664110e3e335a09bbdd2e
romantic <ida.shalilian@gmail.com>
2021-05-16 14:09:32 -0500

    fixed delete

--- controllers/api.js
+++ controllers/api.js
@@ -47,41 +47,19 @@ module.exports = function(app) {
 
     app.delete('/api/notes/:id', (req,res) => {
         deleteMe = parseInt(req.params.id);
-        var data = fs.readFile(DB);
-        var parsed = JSON.parse(data);
+        var data = fs.readFile(DB, 'utf8', function(err,data) {
+            if (err) { console.log (err); return }
+            console.log(data);
+            let parsed = JSON.parse(data);
+            parsed = parsed.filter((parsed) => { return parsed.id !== deleteMe });
+            parsed = JSON.stringify(parsed);
 
-        parsed.filter((parsed) => { return parsed.id !== deleteMe });
-
-        fs.writeFile(DB, JSON.stringify(parsed, null, 2))
-        return res.json(false);
+            fs.writeFile(DB, parsed, err => {
+                if (err) { console.log (err); return }
+                console.log('Note Deleted');
+                parsed = JSON.parse(parsed);
+                res.json(parsed)
+            });
+        });
     });
-        // fs.writeFile(DB, 'utf8', function(err, data) {
-        //     if(err) {
-        //         console.log(err);
-        //         return;
-        //     };
-        //     res.json(deleteNote);
-        // });
-        // const notes = JSON.parse(fs.readFile(DB, 'utf8', {
-        //     if (err) {
-        //         console.log(err)
-        //         return;
-        //    }
-        //    data = JSON.parse(data);
-        //    res.json(data); 
-        // }))
-        //make an empty array and put everybody back in minus the id i wanted to delete
-        //read file -> array -> make constant
-        /* Check each character routeName and see if the same as "chosen"
-         If the statement is true, send the character back as JSON,
-         otherwise tell the user no character was found */
-      
-        // for (let i = 0; i < req.length; i++) {
-        //   if (deleteMe === characters[i].routeName) {
-        //     return res.json(characters[i]);
-        //   }
-        // }
-      
-        // return res.json(false);
-   // });
 }
